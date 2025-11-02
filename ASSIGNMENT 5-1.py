f48175ed0359d0e382a1254b2300fe164e394547
josanmk1-png <josanmk1@gmail.com>
2025-11-02 18:58:26 -0500

    Add files via upload

---
+++ ASSIGNMENT 5-1.py
@@ -0,0 +1,56 @@
+
+a = 1
+b = 2
+c = 5
+
+while a < c:
+    a = a + 1
+    b = b + c
+
+print(a, b, c)
+d = 4
+e = 6
+f = 7
+
+while d > f:
+    d = d + 1
+    e = e - 1
+
+print(d, e, f)
+g = 4
+h = 6
+
+while g < h:
+    g = g + 1
+
+print(g, h)
+j = 2
+k = 5
+n = 9
+
+while j < k:
+    m = 6
+    while m < n:
+        print("Goodbye")
+        m = m + 1
+    j = j + 1
+j = 2
+k = 5
+m = 6
+n = 9
+
+while j < k:
+    while m < n:
+        print("Hello")
+        m = m + 1
+    j = j + 1
+p = 2
+q = 4
+
+while p < q:
+    print("Adios")
+    r = 1
+    while r < q:
+        print("Adios")
+        r = r + 1
+    p = p + 1
